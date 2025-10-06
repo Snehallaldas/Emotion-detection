756a37f24800851476444c061205fd5b3eebd098
Snehallaldas <snehallaldas@gmail.com>
2025-10-07 01:50:52 +0530

    first commit

--- app.py
+++ app.py
@@ -22,3 +22,8 @@ def predict(request: Request, text: str = Form(...)):
     pred = model.predict(transformed)[0]
     label = "😠 Angry" if pred == 0 else "😊 Happy"
     return templates.TemplateResponse("index.html", {"request": request, "prediction": label, "text": text})
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
